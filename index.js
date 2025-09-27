939bd7560eee1aee55693526c8baa8fcd4231ff3
Tenzin Palden <tenpaldenb21@gmail.com>
2025-09-27 13:40:33 +0530

    Fix paths for Render deployment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend files
-app.use(express.static(path.join(__dirname, "../frontend"))); // points to your frontend folder
+app.use(express.static(path.join(__dirname, "./frontend"))); // points to your frontend folder
 
 // API route
 app.get("/api/message", (req, res) => {
@@ -18,7 +18,7 @@ app.get("/api/message", (req, res) => {
 
 // Catch all route to serve index.html for frontend
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
